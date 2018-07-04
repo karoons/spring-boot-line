3313333b2163983c71a98a5aa90a7c60659a5e3b
ANANDA\karoon <karoon@ananda.co.th>
2018-07-04 11:00:43 +0700

    no message

--- src/main/java/com/ananda/controller/MessagingController.java
+++ src/main/java/com/ananda/controller/MessagingController.java
@@ -58,8 +58,8 @@ public class MessagingController {
     @ResponseStatus(HttpStatus.OK)
 //    public void lineWebHook(HttpServletRequest req) throws Exception {
         public void lineWebHook(HttpServletRequest req,@RequestBody  Map<String,Object> input) throws Exception {
-//        System.out.println("-- test input -------"+input.get("events"));
-        printMapDynamic(input);
+        System.out.println("-- values of input -------"+input.values().size());
+//        printMapDynamic(input);
         String json = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
         System.out.println("----json------ "+json);
         lineMessagingService.handleMessage(json,req.getHeader("X-Line-Signature"));
